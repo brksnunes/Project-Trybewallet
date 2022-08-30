ce9c5e2d0260763d65bf51c6c789fbefaf8cec3d
Rodrigo Nunes <brksnunes@protonmail.com>
2022-08-30 20:46:15 -0300

    feat: :sparkles: Added main Header element
    Added main Header element and added placeholder styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../App.css';
 
 class Header extends Component {
   render() {
+    const { email, totalAmount } = this.props;
     return (
-      <div>Header</div>
+      <header className="main-header">
+        <p data-testid="email-field">{ email }</p>
+        <div>
+          <p data-testid="total-field">
+            Despesa total: R$
+            {' '}
+            { totalAmount }
+          </p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  totalAmount: PropTypes.number,
+};
+
+Header.defaultProps = {
+  totalAmount: 0,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
